177416e75d71a75122355c588627d51fbfe85585
justinsta624 <justinsta624@gmail.com>
2024-01-24 19:03:49 -0500

    Update platformsseeds.js
    fix simple errors (genre_name to platform_name)

--- seeds/platformsseeds.js
+++ seeds/platformsseeds.js
@@ -9,55 +9,55 @@ const platformSeedData = [
   },
   {
     id:'2',
-    genre_name: 'Playstation 3',
+    platform_name: 'Playstation 3',
   },
   {
     id:'3',
-    genre_name: 'Playstation 4',
+    platform_name: 'Playstation 4',
   },
   {
     id:'4',
-    genre_name: 'Playstation 5',
+    platform_name: 'Playstation 5',
   },
   {
     id:'5',
-    genre_name: 'XBox',
+    platform_name: 'XBox',
   },
   {
     id:'6',
-    genre_name: 'XBox 360',
+    platform_name: 'XBox 360',
   },
   {
     id:'7',
-    genre_name: 'XBox One',
+    platform_name: 'XBox One',
   },
   {
     id:'8',
-    genre_name: 'XBox Series X',
+    platform_name: 'XBox Series X',
   },
   {
     id:'9',
-    genre_name: 'PC',
+    platform_name: 'PC',
   },
   {
     id:'10',
-    genre_name: 'Nintendo Switch',
+    platform_name: 'Nintendo Switch',
   },
   {
     id:'11',
-    genre_name: 'Android',
+    platform_name: 'Android',
   },
   {
     id:'12',
-    genre_name: 'Apple',
+    platform_name: 'Apple',
   },
   {
     id:'13',
-    genre_name: 'Retro Console',
+    platform_name: 'Retro Console',
   },
   {
     id:'14',
-    genre_name: 'other',
+    platform_name: 'other',
   }
   // Add more platforms as needed
 ];
