15e4f66ed04dadd1cfcef5866651d6210d6dce0b
Ozdal Dogru <ozdaldogru@gmail.com>
2024-02-06 21:49:04 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,20 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// Create a connection object
-const sequelize = new Sequelize(
+let sequelize;
+
+if (process.env.CLEARDB_URL) {
+  sequelize = new Sequelize(process.env.CLEARDB_URL);
+} else {
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {decimalNumbers: true,},
+      port: 3306
     }
-    );
-    
+  );
+}
 module.exports = sequelize;
