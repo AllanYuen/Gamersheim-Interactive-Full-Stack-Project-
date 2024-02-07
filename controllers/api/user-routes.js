4579957e2a7a9df4550c224351a07f8c07e7a97c
Ozdal Dogru <ozdaldogru@gmail.com>
2024-02-06 21:40:35 -0500

    controllers/connection file, server.js and login.js updated

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -25,40 +25,67 @@ router.post('/', async (req, res) => {
 });
 
 
-
-// Login
 router.post('/login', async (req, res) => {
   try {
-    const dbUserData = await Users.findByPk(req.body.id);
-
-    if (!dbUserData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email, please try again' });
-      return;
-    }
-
-    const validPassword = await dbUserData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect password, please try again' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = dbUserData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
-    });
-
+      const dbUserData = await User.findOne({
+          where: {id: req.body.id}
+      });
+      if (!dbUserData) {
+          res.status(400).json({ message: `User id is not valid.` });
+          return;
+      }
+      // check pw
+      const pwValidated = await dbUserData.checkPassword(req.body.password)
+      if (!pwValidated) {
+          res.status(400).json({ message: "Incorrect password!" });
+          return;
+      }
+      // create session and send response back
+      req.session.save(() => {
+          req.session.id = dbUserData.id;
+          req.session.email = dbUserData.email;
+          req.session.loggedIn = true;        
+      //send response to client
+      res.status(200).json({ message: "You are logged in!" });
+      });
   } catch (err) {
-    res.status(400).json(err);
+      res.status(400).json(err);
   }
 });
 
+/*
+// Login //
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (![email, password].every((item) => item.trim() !== "")) {
+      res.status(404).send({ message: "Invalid parameters" });
+      return;
+    }
+
+    const user = await Users.findOne({where: { email },});
+
+    if (!user) {res.status(404).json({ message: "Invalid email" });
+      return;
+    }
+
+    if (!(await user.checkPassword(password))) {res.status(404).json({ message: "Invalid password" });
+      return;
+    }
+
+    const {email: userEmail,} = user.get({plain: true,});
+
+
+    res.status(200).json({
+      status: "success",
+      user: { email: userEmail },
+    });
+  } catch (e) {
+    res.status(500).json({ message: "NOOOOOOOOOO" });
+  }
+});
+*/
 
 
 // Logout
--- public/js/login.js
+++ public/js/login.js
@@ -1,46 +1,54 @@
 // Login Form
 const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
+  event.preventDefault();
 
-    const email = document.querySelector('#emaillogin').value.trim();
-    const password = document.querySelector('#passwordlogin').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('.api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-      if (response.ok) {document.location.replace('/');
-      } 
-      else {alert('something is wrong');}}};
+  const email = document.querySelector("#emaillogin").value.trim();
+  const password = document.querySelector("#passwordlogin").value.trim();
 
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 
-  
+  if (email || password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("something is wrong");
+    }
+  }
+};
+
+document.querySelector(".login-form").addEventListener("submit", loginFormHandler);
+
+
+
 //sign in form
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const firstname = document.querySelector('#firstnamesignup').value.trim();
-    const lastname = document.querySelector('#lastnamesignup').value.trim();  
-    const email = document.querySelector('#emailsignup').value.trim();
-    const password = document.querySelector('#passwordsignup').value.trim();
-  
-    if (firstname && lastname && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ firstname, lastname, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {document.location.replace('/');} 
-      else {alert(response.statusText);}
-    }
-  };
-  
+  event.preventDefault();
 
-  
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-  
+  const firstname = document.querySelector("#firstnamesignup").value.trim();
+  const lastname = document.querySelector("#lastnamesignup").value.trim();
+  const email = document.querySelector("#emailsignup").value.trim();
+  const password = document.querySelector("#passwordsignup").value.trim();
+
+  if (firstname && lastname && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ firstname, lastname, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
